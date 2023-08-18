e1f9f010c994230c28f9d078e88e7e77813aa521
Kira Beloludinova <102582189+KiraBelo@users.noreply.github.com>
2023-08-18 20:16:08 +0300

    final lint

--- src/view.js
+++ src/view.js
@@ -4,104 +4,104 @@ import renderPosts from './renderPosts.js';
 import renderModal from './renderModal.js';
 
 const clear = (elements) => {
-    const { input, feedback, button } = elements;
-    feedback.classList.remove('text-danger');
-    feedback.classList.remove('text-warning');
-    feedback.classList.remove('text-success');
-    input.classList.remove('is-invalid');
+  const { input, feedback, button } = elements;
+  feedback.classList.remove('text-danger');
+  feedback.classList.remove('text-warning');
+  feedback.classList.remove('text-success');
+  input.classList.remove('is-invalid');
 
-    input.disabled = false;
-    button.disabled = false;
+  input.disabled = false;
+  button.disabled = false;
 };
 
 const handleError = (errorMessage, elements, i18next) => {
-    const { feedback } = elements;
-    feedback.textContent = i18next.t(`errors.${errorMessage}`);
+  const { feedback } = elements;
+  feedback.textContent = i18next.t(`errors.${errorMessage}`);
 };
 
 const handleForm = (state, elements, i18next) => {
-    const {
-        input,
-        feedback,
-        form,
-        button,
-    } = elements;
-    const { status } = state.form;
-    clear(elements);
+  const {
+    input,
+    feedback,
+    form,
+    button,
+  } = elements;
+  const { status } = state.form;
+  clear(elements);
 
-    switch (status) {
-        case 'loading':
-            {
-                feedback.classList.add('text-warning');
-                feedback.textContent = i18next.t(`status.${status}`);
-                input.disabled = true;
-                button.disabled = true;
-                break;
-            }
-        case 'success':
-            {
-                feedback.classList.add('text-success');
-                feedback.textContent = i18next.t(`status.${status}`);
-                form.reset();
-                input.focus();
-                break;
-            }
-        case 'failed':
-            {
-                feedback.classList.add('text-danger');
-                input.classList.add('is-invalid');
-                feedback.textContent = i18next.t(`errors.${[state.form.error]}`);
-                break;
-            }
-        default:
-            break;
+  switch (status) {
+    case 'loading':
+    {
+      feedback.classList.add('text-warning');
+      feedback.textContent = i18next.t(`status.${status}`);
+      input.disabled = true;
+      button.disabled = true;
+      break;
     }
+    case 'success':
+    {
+      feedback.classList.add('text-success');
+      feedback.textContent = i18next.t(`status.${status}`);
+      form.reset();
+      input.focus();
+      break;
+    }
+    case 'failed':
+    {
+      feedback.classList.add('text-danger');
+      input.classList.add('is-invalid');
+      feedback.textContent = i18next.t(`errors.${[state.form.error]}`);
+      break;
+    }
+    default:
+      break;
+  }
 };
 
 const renderVisitedPosts = (idVisitedPosts) => {
-    idVisitedPosts.forEach((id) => {
-        const link = document.querySelector(`a[data-id="${id}"]`);
-        link.classList.remove('fw-bold');
-        link.classList.add('fw-normal', 'link-secondary');
-    });
+  idVisitedPosts.forEach((id) => {
+    const link = document.querySelector(`a[data-id="${id}"]`);
+    link.classList.remove('fw-bold');
+    link.classList.add('fw-normal', 'link-secondary');
+  });
 };
 
 const watch = (state, elements, i18nextInstance) => onChange(state, (path, value) => {
-    console.log('watch', path, value);
-    switch (path) {
-        case 'form.status':
-            {
-                handleForm(state, elements, i18nextInstance);
-                break;
-            }
-        case 'form.error':
-            {
-                handleError(value, elements, i18nextInstance);
-                break;
-            }
-        case 'posts':
-            {
-                renderPosts(state, value, elements, i18nextInstance);
-                break;
-            }
-        case 'feeds':
-            {
-                renderFeeds(value, elements, i18nextInstance);
-                break;
-            }
-        case 'idVisitedPosts':
-            {
-                renderVisitedPosts(value);
-                break;
-            }
-        case 'idCurrentPost':
-            {
-                renderModal(state, value);
-                break;
-            }
-        default:
-            break;
+  console.log('watch', path, value);
+  switch (path) {
+    case 'form.status':
+    {
+      handleForm(state, elements, i18nextInstance);
+      break;
     }
+    case 'form.error':
+    {
+      handleError(value, elements, i18nextInstance);
+      break;
+    }
+    case 'posts':
+    {
+      renderPosts(state, value, elements, i18nextInstance);
+      break;
+    }
+    case 'feeds':
+    {
+      renderFeeds(value, elements, i18nextInstance);
+      break;
+    }
+    case 'idVisitedPosts':
+    {
+      renderVisitedPosts(value);
+      break;
+    }
+    case 'idCurrentPost':
+    {
+      renderModal(state, value);
+      break;
+    }
+    default:
+      break;
+  }
 });
 
 export default watch;
